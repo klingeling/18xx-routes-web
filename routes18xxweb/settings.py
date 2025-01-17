3f2478a14cb01293fbe21521705454a3aacedaec
klingeling <915157650@qq.com>
2025-01-17 15:59:38 +0800

    try zh

--- routes18xxweb/settings.py
+++ routes18xxweb/settings.py
@@ -10,4 +10,5 @@ WTF_CSRF_ENABLED = True
 WTF_CSRF_TIME_LIMIT = None
 
 # SendGrid
-MAIL_USERNAME = os.environ['EMAIL_USER']
+# MAIL_USERNAME = os.environ['EMAIL_USER']
+MAIL_USERNAME = '915157650@qq.com'
